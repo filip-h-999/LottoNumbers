f6644827dd084ffa53b7a8726397071b4f596089
filip-h-999 <filip.hrenovcik@gmail.com>
2024-01-19 20:55:08 +0100

    Add border and border-radius to body element, and implement random number generation functionality

---
+++ static/main.js
@@ -0,0 +1,22 @@
+function generateRandomNumbers(min, max, count) {
+	const numbers = [];
+	for (let i = 0; i < count; i++) {
+		const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+		numbers.push(randomNumber);
+	}
+	return numbers;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const numbersElement = document.querySelectorAll("#number1, #number2, #number3, #number4, #number5, #number6");
+	const button = document.getElementById("button-19");
+
+    button.addEventListener("click", () => {
+        const randomNumbers = generateRandomNumbers(1, 49, 6);
+        randomNumbers.forEach((number, index) => {
+            numbersElement[index].textContent = number;
+        });
+    });
+
+	console.log(generateRandomNumbers(1, 49, 6));
+});
